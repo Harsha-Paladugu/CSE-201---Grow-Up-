4c1916162e57e662a8bdfca90f56e07b17f02c97
Harsha-Paladugu <163939623+Harsha-Paladugu@users.noreply.github.com>
2024-10-30 13:59:16 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,12 @@
+// Initialize a new player at start of game
+// add methods for event affecting player
+public class Player {
+    
+    public void createPlayer() {
+        Age age = new Age();
+        Money money = new Money();
+        Athletics athletics = new Athletics();
+        Education education = new Education();
+        Karma karma = new Karma();
+    }
+}
