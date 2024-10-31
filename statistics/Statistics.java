750c1a95b956ab802d4463c1f54cd9d24e6d0135
Harsha-Paladugu <163939623+Harsha-Paladugu@users.noreply.github.com>
2024-10-31 00:40:49 -0400

    Update Statistics.java

--- statistics/Statistics.java
+++ statistics/Statistics.java
@@ -1,21 +1,42 @@
+/**
+ * Represents a base class for tracking various player statistics 
+ * throughout the game.
+ */
 public class Statistics {
-    
+
+    /**
+     * Holds the player's statistic value. Initially set to 0.
+     */
     protected int stat;
-    
+
+    /**
+     * Initializes the statistic to zero.
+     */
     public Statistics() {
         this.stat = 0;
     }
-    
+
+    /**
+     * Increases the statistic by a specified value.
+     * @param value the amount to add to the statistic.
+     */
     public void statUp(int value) {
         stat += value;
     }
-    
+
+    /**
+     * Decreases the statistic by a specified value.
+     * @param value the amount to subtract from the statistic.
+     */
     public void statDown(int value) {
         stat -= value;
     }
-    
+
+    /**
+     * Retrieves the current statistic value.
+     * @return the current value of the statistic.
+     */
     public int getStat() {
         return stat;
     }
-
 }
