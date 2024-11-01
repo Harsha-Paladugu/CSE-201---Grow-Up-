7b2d691560c7b3b66e2c9d08efedb326174407bf
soplittle <soplittle@gmail.com>
2024-11-01 11:24:27 -0400

    java doc Menu.java

--- statistics/Menu.java
+++ statistics/Menu.java
@@ -1,14 +1,25 @@
 import java.util.List;
 
+/**
+* This class displays all of the statistics
+*/
+
 public class Menu {
+    
     private List<Statistics> statistics;
 
-    // Constructor to initialize all statistics
+    /**
+    * constructs a new menu object with the stastics
+    */
+    
     public Menu(List<Statistics> statistics) {
         this.statistics = statistics;
     }
 
-    // Show all statistics
+    /**
+    * Displays all the statistics in this list with the statistic name and value
+    */
+    
     public void showStats() {
         System.out.println("\nAll Stats:");
         for (Statistics stat : statistics) {
