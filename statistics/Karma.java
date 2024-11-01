20d399649a38c7feda0a189570231485cd3355b0
soplittle <soplittle@gmail.com>
2024-11-01 10:57:17 -0400

    javadoc Karma.java

--- statistics/Karma.java
+++ statistics/Karma.java
@@ -1,14 +1,24 @@
+/**
+ * The Karma class represents a karma statistic and extends the Statistics class.
+ */
 
 public class Karma extends Statistics{
+
+    /**
+     * Constructs a new karma object with default values by calling the superclass constructor.
+     */
         
         public Karma() {
             super();
         }
-
+    /**
+     * Returns the name of the statistic as a string.
+     *
+     * @return the name of this statistic, which is "Karma".
+     */
+        
         @Override
         public String getStatName() {
             return "Karma";
         }
-
-        
 }
