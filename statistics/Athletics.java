a8ab44a6f937d92d9380654199f814532a25672c
soplittle <soplittle@gmail.com>
2024-11-01 10:47:33 -0400

    Update Athletics.java

--- statistics/Athletics.java
+++ statistics/Athletics.java
@@ -1,12 +1,24 @@
+/**
+* The athletics class represents the athletics stats the player has. 
+* It extends the Statistics class
+*/
+public class Athletics extends Statistics {
 
-public class Athletics extends Statistics{
+    /**
+    * Constructs a new athletics object using the superclass constructor.
+    */
     
     public Athletics() {
         super();
     }
+
+    /**
+    * Returns the name of Athletics as a string
+    *
+    * @return the name of the statistics "Athletics"
     
     @Override
     public String getStatName() {
-        return "Athletics()";
+        return "Athletics";
     }
 }
