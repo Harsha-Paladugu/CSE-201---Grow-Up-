b0766d860e1a0b265633287df3903bb4c9068780
Harsha-Paladugu <163939623+Harsha-Paladugu@users.noreply.github.com>
2024-11-08 09:29:48 -0500

    Add files via upload

--- Event.java
+++ Event.java
@@ -1,147 +1,43 @@
+import java.util.Random;
+
+/**
+ * The Event class checks if a random event should happen based on a probability
+ * and the player's karma level.
+ */
 public class Event {
 
-    /**
-     * This method selects and returns a room appropriate random event
-     * @param room The current room the player is in
-     * @return An array containing the event and its effects on the player
-     */
-    protected String[] randomEvent(int room, int karma){
-        String[] results = new String[5];
-        int eventNum = getEventNum(room, karma);
-
-        //For clarity Each room is represented by an if statement
-        //While within each room events are chosen in a switch statement
-
-        if(room == 0){
-            switch (eventNum){
-                case 0:
-                    results[0] = "";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-
-                case 1:
-                    results[0] = "You were adopted";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-
-                case 2:
-                    results[0] = "Your parents chose not to vaccinate you";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-            }
-        }
-        if(room == 1){
-            switch (eventNum){
-                case 0:
-                    results[0] = "event";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-            }
-        }
-        if(room == 2){
-            switch (eventNum){
-                case 0:
-                    results[0] = "event";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-            }
-        }
-        if(room == 3){
-            switch (eventNum){
-                case 0:
-                    results[0] = "event";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-            }
-        }
-        if(room == 4){
-            switch (eventNum){
-                case 0:
-                    results[0] = "event";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-            }
-        }
-        if(room == 5){
-            switch (eventNum){
-                case 0:
-                    results[0] = "event";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-            }
-        }
-        if(room == 6){
-            switch (eventNum){
-                case 0:
-                    results[0] = "event";
-                    results[1] = "+KAR0";
-                    results[2] = "+MON0";
-                    results[3] = "+EDU0";
-                    results[4] = "+ATH0";
-                    break;
-            }
-        }
-
-        return results;
-    }
+    private static Random random = new Random();
 
     /**
-     * This method returns an int within the range of events for a given room
-     * @param room The current room the player is in
-     * @return an int to use for a random event
+     * Checks if an event should happen based on the given probability.
+     * If the player's karma is below 0, the event will not happen.
+     * If karma is 0 or more, the event has a chance to happen based on the probability.
+     *
+     * @param player The player whose karma affects the event.
+     * @param probability The chance (0-100) that the event happens if karma is high enough.
+     * @return true if the event happens, false if it does not.
      */
-    private int getEventNum(int room, int karma){
-        int eventNum = -1;
-
-        //If player Karma is low, the worst outcome is guaranteed
-        if (karma<-10){
-            return 1;
+    public boolean eventOccuring(Player player, int probability) {
+        
+        // Check if karma is too low for the event to happen
+        if (isKarmaLow(player)) {
+            return false;
         }
 
-        //chooses the int for an event that can happen in a room
-        switch (room){
-            case 0:
-                 eventNum = (int) (Math.random() * (2));
-                 break;
-            case 1:
-                eventNum = (int) (Math.random() * (6));
-                break;
-            case 2:
-                eventNum = (int) (Math.random() * (6));
-                break;
-            case 3:
-                eventNum = (int) (Math.random() * (1));
-                break;
-            case 4:
-                eventNum = (int) (Math.random() * (1));
-                break;
-
-        }
-        return eventNum;
+        // Generate a random number between 0 and 99
+        int randomValue = random.nextInt(100);
+        
+        // Return true if randomValue is less than the probability, indicating the event happens
+        return randomValue < probability;
     }
-
+    
+    /**
+     * Checks if the player's karma is below the level needed for the event to happen.
+     *
+     * @param player The player whose karma is checked.
+     * @return true if the player's karma is below 20, otherwise false.
+     */
+    public boolean isKarmaLow(Player player) {
+        return player.karma.getStatValue() < 0;
+    }
 }
