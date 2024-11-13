a93711a79a58461b4a3d839688d206904a2c0743
Harsha-Paladugu <163939623+Harsha-Paladugu@users.noreply.github.com>
2024-11-13 00:56:51 -0500

    Add files via upload

--- stages/InfancyStage.java
+++ stages/InfancyStage.java
@@ -2,36 +2,30 @@
 public class InfancyStage {
 
     public static void playInfancyStage(Player player) {
-        int[] positiveStats = new int[5];
-        int[] negativeStats = new int[5];
-        
-        // start infancy stage
-        System.out.println("Infancy Stage: \n\n");
-        System.out.println("You have been born.\n");
 
         // start adoption random event
-        positiveStats = Control.setStatArray(1, 0, 0, -10, 0);
-        negativeStats = Control.setStatArray(1, 0, 0, 0, 0);
+        Main.positiveStats = Control.setStatArray(1, 0, 0, -10, 0);
+        Main.negativeStats = Control.setStatArray(1, 0, 0, 0, 0);
         
         Control.randomEventHandler(
                 "The doctor has given your parents a form to give you up for adoption.", // initial message
                 "Your parents put you up for adoption.\n\nYou were adopted by a new family.", // message if event happens
-                positiveStats, // stat changes if event happens
+                Main.positiveStats, // stat changes if event happens
                 "Your parents decided to raise you.", // message if event does not happen
-                negativeStats, // stat changes if event does not happen
+                Main.negativeStats, // stat changes if event does not happen
                 50, // probability of event happening
                 player); // end adoption event
         
 
         // start vaccination random event
-        positiveStats = Control.setStatArray(1, 0, 0, 10, 0);
-        negativeStats = Control.setStatArray(1, 0, -10, -10, 0);
+        Main.positiveStats = Control.setStatArray(1, 0, 0, 10, 0);
+        Main.negativeStats = Control.setStatArray(1, 0, -10, -10, 0);
         
         Control.randomEventHandler("It is time for your vaccinations, will your parents choose to vaccinate you?", 
                 "Your parents chose to vaccinate you.", 
-                positiveStats, 
+                Main.positiveStats, 
                 "Your parents decided to not vaccinate you.", 
-                negativeStats, 
+                Main.negativeStats, 
                 50, 
                 player); // end vaccination event      
     }
--- stages/KindergartenStage.java
+++ stages/KindergartenStage.java
@@ -1,9 +1,6 @@
 public class KindergartenStage {
     
     public static void playKindergartenStage(Player player) {
-        
-        int[] positiveStats = new int[5];
-        int[] negativeStats = new int[5];
             
         System.out.println("Starting Kindergarten");
     
@@ -11,34 +8,89 @@ public class KindergartenStage {
         player.age.changeStat(5); 
     
         // Start friendship event
-        positiveStats = Control.setStatArray(1,0,0,10,0);
-        negativeStats = Control.setStatArray(1, 0, 0, -10, 0); // amount to change age, athletics, education, karma, money.
+        Main.positiveStats = Control.setStatArray(1,0,0,10,0);
+        Main.negativeStats = Control.setStatArray(1, 0, 0, -10, 0); // amount to change age, athletics, education, karma, money.
         
-        Control.CHOICE playerChoice = Control.choiceEventHandler( // collect playerChoice for lunch event
+        Main.playerChoice = Control.choiceEventHandler( // collect playerChoice for lunch event
                 "It's the first day of kindergarten, you see many people in your class.", // Initial message
                 "Do you want to make a friend?", // Prompt Player Yes or No
                 "You made a friend!", // Message if Yes
-                positiveStats, // Stat changes if Yes
+                Main.positiveStats, // Stat changes if Yes
                 "You kept to yourself.", // Message if No
-                negativeStats, // Stat changes if No
+                Main.negativeStats, // Stat changes if No
                 player); // End friendship event
     
         // Start lunch event
-        positiveStats = Control.setStatArray(1, 0, 0, 20, 0);
-        negativeStats = Control.setStatArray(1, 0, 0, -10, 0);
+        Main.positiveStats = Control.setStatArray(1, 0, 0, 20, 0);
+        Main.negativeStats = Control.setStatArray(1, 0, 0, -10, 0);
         
-        if(playerChoice == Control.CHOICE.YES) {
+        if(Main.playerChoice == Control.CHOICE.YES) {
             
             Control.choiceEventHandler(
                     "You are eating lunch with your friend, he asks to try your pretzels.",
                     "Do you want to give him some of your pretzels?",
                     "You gave your friend some of your pretzels.",
-                    positiveStats,
+                    Main.positiveStats,
                     "You did not give your friend any pretzels.",
-                    negativeStats,
+                    Main.negativeStats,
                     player);
         } else {
-            player.age.changeStat(positiveStats[0]);
+            player.age.changeStat(Main.positiveStats[0]);
         } // End lunch event
+        
+        // Start random fight event
+        Main.positiveStats = Control.setStatArray(1, 0, 0, -10, 0);
+        Main.negativeStats = Control.setStatArray(1, 0, 0, 10, 0);
+        
+        Control.randomEventHandler(
+                "During recess, a kid bumps into you.", // Initial Message
+                "You stood up for yourself and got into a small fight.", // Message if event happens
+                Main.positiveStats, // Stat changes if event happens
+                "You let it go and avoided any trouble.", // Message if event does not happen
+                Main.negativeStats, // Stat changes if event does not happen
+                0, // probability
+                player); // End random fight event 
+        
+        // Start sports event
+        Main.positiveStats = Control.setStatArray(1, 25, 0, 0, 0);
+        Main.negativeStats = Control.setStatArray(1, 0, 0, 0, 0);
+        
+        Control.choiceEventHandler(
+                "The school is looking for students to join the football team.", // Initial message
+                "Do you want to sign up for football?", // Prompt for Yes/No choice
+                "You joined the football team! Your athletic skills improved.", // Message if Yes
+                Main.positiveStats, // Stat changes if Yes
+                "You decided not to join the football team.", // Message if No
+                Main.negativeStats, // Stat changes if No
+                player); // End sports event
+        
+        // Start chores event
+        Main.positiveStats = Control.setStatArray(1, 0, 0, 10, 50);
+        Main.negativeStats = Control.setStatArray(1, 0, 0, -5, 0);
+
+        Main.playerChoice = Control.choiceEventHandler(
+                "Your parents ask if you can help with chores around the house.", // Initial message
+                "Do you want to help with the chores?", // Prompt Yes or No
+                "You helped with the chores and earned some allowance!", // Message if Yes
+                Main.positiveStats, // Stat changes if Yes
+                "You chose not to help with the chores.", // Message if No
+                Main.negativeStats, // Stat changes if No
+                player); // End chores event
+        
+     // Start pet event
+        Main.positiveStats = Control.setStatArray(1, 0, 0, 15, -30); // Increase karma and decrease money if they get a pet
+        Main.negativeStats = Control.setStatArray(1, 0, 0, 0, 0); // No change if they choose not to
+
+        if (player.money.getStatValue() > 30) { // Run event if player has enough money to adopt a pet
+            
+            Control.choiceEventHandler(
+                "You have the opportunity to adopt a pet.", // Initial message
+                "Do you want to get a pet?", // Prompt for Yes/No choice
+                "You adopted a pet! It brings you joy but comes with expenses.", // Message if Yes
+                Main.positiveStats, // Stat changes if Yes
+                "You decided not to get a pet.", // Message if No
+                Main.negativeStats, // Stat changes if No
+                player); // End pet event
+        } 
     }
 }
